a19467a0b6a7032e6f0b9e10c4dc67b6083b9fbb
Yankee <popularlol786@gmail.com>
2022-02-06 19:16:09 +0800

    added music functionality

--- cogs/music.py
+++ cogs/music.py
@@ -1,12 +1,71 @@
 from discord.ext import commands
+from discord import FFmpegPCMAudio
+import youtube_dl
+import discord
+import asyncio
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
 class music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        print(message.content)
+        
+    @commands.command()
+    async def play(self, ctx, url):
+        user_vc = ctx.author.voice
+        if user_vc == None:
+            await ctx.send("You must join a voice channel first.")
+
+        vc = user_vc.channel
+        vc = await vc.connect()
+
+        player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+        ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+
+        await ctx.send(f'Now playing: {player.title}')
+        
+
+    
 
 
 def setup(bot):
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-
+from discord import FFmpegPCMAudio
 from discord.ext import commands
 
 def get_token():
