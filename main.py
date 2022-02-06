1179b31c20008246eca20585a5e24309e7243b16
Yankee <popularlol786@gmail.com>
2022-02-06 14:30:11 +0800

    Fixed no readline() for token get_token

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import discord
 
 from discord.ext import commands
 
+def get_token():
+    with open("token.txt", "r") as token:
+        return token.readline()    
+
 class MusicBot(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix='!', help_command=None, description=None)
+        self.load_extension("cogs.music")
 
     async def on_ready(self):
         print(f"logged in as {self.user}")
 
 test = MusicBot()
-test.run("")
+test.run(get_token())
